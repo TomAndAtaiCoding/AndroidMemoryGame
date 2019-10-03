5d96313addb14e5d45b63d8fc722839a0d4c73d7
Atai Ambus <atayambus@gmail.com>
2019-10-03 15:49:09 +0300

    initial commit

---
+++ AndroidMemoryGame/app/src/main/java/com/example/androidmemorygame/TableGame.java
@@ -0,0 +1,14 @@
+package com.example.androidmemorygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TableGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_game);
+    }
+}
